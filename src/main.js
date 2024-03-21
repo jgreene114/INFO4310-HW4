8dce94877ba8a23e4b3af99858e1298dc9b875b3
jgreene114 <83301570+jgreene114@users.noreply.github.com>
2024-03-20 22:07:57 -0400

    cleaned data, created trips and hubs csvs

--- src/main.js
+++ src/main.js
@@ -1,51 +1,63 @@
 var map;
+const minZoom = 12,
+    maxZoom = 20,
+    initZoom = 12
+
 
 const createMap = function () {
-    map = L.map('map').setView([45.5231, -122.6765], 12.);
+    map = L.map('map').setView([45.5231, -122.6765], initZoom);
 
     // found on https://leaflet-extras.github.io/leaflet-providers/preview/index.html
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd', // QUESTION: what is this line?
-        maxZoom: 20,
-        minZoom: 10
+        maxZoom: maxZoom,
+        minZoom: minZoom
     }).addTo(map);
-    const topLeft = L.latLng(52, -132.54)
-    const bottomRight = L.latLng(21.78, -59.94)
-    const bBox = L.latLngBounds(topLeft, bottomRight)
+
+    const bBox = L.latLngBounds(
+        L.latLng(45.4325, -122.8367),
+        L.latLng(45.6529, -122.4720)
+    )
     map.setMaxBounds(bBox)
 }
 
 const loadData = async function () {
-    var tripData = await d3.csv("data/Biketown Raw Data/2016_07.csv")
+    var hubData = await d3.csv("data/Biketown Cleaned Data/hubs.csv")
+    var tripData = await d3.csv("data/Biketown Cleaned Data/trips.csv")
 
-    tripData = tripData.filter(d => {
-        const startLatitude = parseFloat(d.StartLatitude);
-        const startLongitude = parseFloat(d.StartLongitude);
-        const endLatitude = parseFloat(d.EndLatitude);
-        const endLongitude = parseFloat(d.EndLongitude);
-        // console.log(d)
-        return !isNaN(startLatitude) && !isNaN(startLongitude) && !isNaN(endLatitude) && !isNaN(endLongitude)
-    })
-        .slice(0, 100)
+    // tripData = hubdata.filter(d => {
+    //     const startLatitude = parseFloat(d.StartLatitude);
+    //     const startLongitude = parseFloat(d.StartLongitude);
+    //     const endLatitude = parseFloat(d.EndLatitude);
+    //     const endLongitude = parseFloat(d.EndLongitude);
+    //     // console.log(d)
+    //     return !isNaN(startLatitude) && !isNaN(startLongitude) && !isNaN(endLatitude) && !isNaN(endLongitude)
+    // })
+    //     .slice(0, 100)
     // console.log("Loaded data: ", tripData);
 
     L.svg({clickable: true}).addTo(map)
-
 
 
     const overlay = d3.select(map.getPanes().overlayPane)
     const svg = overlay.select('svg').attr("pointer-events", "auto")
     const g = svg.append('g')
         .attr('class', 'points leaflet-zoom-hide')
+    // question: try removing zoom hide?
 
     const fillOpacity = .1
     const strokeOpacity = 1
-    const normalRadius = 6
-    const hoverRadius = 9
+
+    const radiusScale = d3.scaleLinear()
+        .domain([minZoom, maxZoom])
+        .range([2,8]);
+    const hoverRadiusScale = d3.scaleLinear()
+        .domain([minZoom, maxZoom])
+        .range([2*1.5, 8*1.5]);
 
     // console.log("adfdsf")
-    tripData.forEach(d => {
+    hubData.forEach(d => {
         const startPoint = map.latLngToLayerPoint(new L.LatLng(d.StartLatitude, d.StartLongitude));
         const endPoint = map.latLngToLayerPoint(new L.LatLng(d.EndLatitude, d.EndLongitude));
         
@@ -59,26 +71,34 @@ const loadData = async function () {
             .attr("fill-opacity", fillOpacity)
             .attr("stroke", primaryColor)
             .attr("stroke-opacity", strokeOpacity)
-            .attr("r", normalRadius)
+            .attr("r", radiusScale(initZoom))
             .attr("cx", startPoint.x)
             .attr("cy", startPoint.y)
             .on('mouseover', function () {
                 d3.select(this)
+                    .style('cursor', 'pointer')
                     .transition()
                     .duration(150)
                     .attr("fill", hoverColor)
                     .attr("fill-opacity", 0.5)
                     .attr("stroke", hoverColor)
-                    .attr('r', hoverRadius);
+                    // .attr('r', radiusScale(initZoom) * hoverRadiusMultiplier * 1.5)
+                    .attr('r', hoverRadiusScale(initZoom) * 1.5)
+                    .transition()
+                    .ease(d3.easeExpOut)
+                    .duration(150)
+                    .attr('r', hoverRadiusScale(initZoom))
+                    // .attr('r', radiusScale(initZoom) * hoverRadiusMultiplier)
             })
             .on('mouseout', function () {
                 d3.select(this)
+                    .style('cursor', 'grab')
                     .transition()
                     .duration(150)
                     .attr("fill", primaryColor)
                     .attr("fill-opacity", fillOpacity)
                     .attr("stroke", primaryColor)
-                    .attr('r', normalRadius);
+                    .attr('r', radiusScale(initZoom));
             });
 
         primaryColor = colorPalette['complement'][0]
@@ -87,46 +107,112 @@ const loadData = async function () {
 
         g.append('circle')
             .datum(d)
-            .attr("class", "point start-point")
+            .attr("class", "point end-point")
             .attr("fill", primaryColor)
             .attr("fill-opacity", fillOpacity)
             .attr("stroke", primaryColor)
             .attr("stroke-opacity", strokeOpacity)
-            .attr("r", normalRadius)
-            .attr("cx", startPoint.x)
-            .attr("cy", startPoint.y)
+            .attr("r", radiusScale(initZoom))
+            .attr("cx", endPoint.x)
+            .attr("cy", endPoint.y)
             .on('mouseover', function () {
                 d3.select(this)
+                    .style('cursor', 'pointer')
                     .transition()
                     .duration(150)
                     .attr("fill", hoverColor)
                     .attr("fill-opacity", 0.5)
                     .attr("stroke", hoverColor)
-                    .attr('r', hoverRadius*1.5)
+                    // .attr('r', radiusScale(initZoom) * hoverRadiusMultiplier * 1.5)
+                    .attr('r', hoverRadiusScale(initZoom) * 1.5)
                     .transition()
                     .ease(d3.easeExpOut)
                     .duration(150)
-                    .attr('r', hoverRadius);
-
+                    .attr('r', hoverRadiusScale(initZoom))
+                    // .attr('r', radiusScale(initZoom) * hoverRadiusMultiplier)
             })
             .on('mouseout', function () {
                 d3.select(this)
+                    .style('cursor', 'grab')
                     .transition()
                     .duration(150)
                     .attr("fill", primaryColor)
                     .attr("fill-opacity", fillOpacity)
                     .attr("stroke", primaryColor)
-                    .attr('r', normalRadius);
+                    .attr('r', radiusScale(initZoom));
             });
     })
 
     const update = () => {
+        const currentZoom = map.getZoom();
+
+        let primaryColor = colorPalette['primary'][0]
+        let hoverColor = colorPalette['primary'][3]
+        
         g.selectAll('circle.start-point')
             .attr("cx", d => map.latLngToLayerPoint(new L.LatLng(d.StartLatitude, d.StartLongitude)).x)
-            .attr("cy", d => map.latLngToLayerPoint(new L.LatLng(d.StartLatitude, d.StartLongitude)).y);
+            .attr("cy", d => map.latLngToLayerPoint(new L.LatLng(d.StartLatitude, d.StartLongitude)).y)
+            .attr('r', radiusScale(currentZoom))
+            .on('mouseover', function () {
+                d3.select(this)
+                    .style('cursor', 'pointer')
+                    .transition()
+                    .duration(150)
+                    .attr("fill", hoverColor)
+                    .attr("fill-opacity", 0.5)
+                    .attr("stroke", hoverColor)
+                    .attr('r', hoverRadiusScale(currentZoom) * 1.5)
+                    // .attr('r', radiusScale(currentZoom) * hoverRadiusMultiplier * 1.5)
+                    .transition()
+                    .ease(d3.easeExpOut)
+                    .duration(150)
+                    .attr('r', hoverRadiusScale(currentZoom))
+                    // .attr('r', radiusScale(currentZoom) * hoverRadiusMultiplier)
+            })
+            .on('mouseout', function () {
+                d3.select(this)
+                    .style('cursor', 'grab')
+                    .transition()
+                    .duration(150)
+                    .attr("fill", primaryColor)
+                    .attr("fill-opacity", fillOpacity)
+                    .attr("stroke", primaryColor)
+                    .attr('r', radiusScale(currentZoom));
+            });
+        
+        primaryColor = colorPalette['complement'][0]
+        hoverColor = colorPalette['complement'][3]
+        
         g.selectAll('circle.end-point')
             .attr("cx", d => map.latLngToLayerPoint(new L.LatLng(d.EndLatitude, d.EndLongitude)).x)
-            .attr("cy", d => map.latLngToLayerPoint(new L.LatLng(d.EndLatitude, d.EndLongitude)).y);
+            .attr("cy", d => map.latLngToLayerPoint(new L.LatLng(d.EndLatitude, d.EndLongitude)).y)
+            .attr('r', radiusScale(currentZoom))
+            .on('mouseover', function () {
+                d3.select(this)
+                    .style('cursor', 'pointer')
+                    .transition()
+                    .duration(150)
+                    .attr("fill", hoverColor)
+                    .attr("fill-opacity", 0.5)
+                    .attr("stroke", hoverColor)
+                    .attr('r', hoverRadiusScale(currentZoom) * 1.5)
+                    .transition()
+                    .ease(d3.easeExpOut)
+                    .duration(150)
+                    .attr('r', hoverRadiusScale(currentZoom))
+            })
+            .on('mouseout', function () {
+                d3.select(this)
+                    .style('cursor', 'grab')
+                    .transition()
+                    .duration(150)
+                    .attr("fill", primaryColor)
+                    .attr("fill-opacity", fillOpacity)
+                    .attr("stroke", primaryColor)
+                    .attr('r', radiusScale(currentZoom));
+            });
+        
+        
     }
 
     map.on("zoomend viewreset", update);
